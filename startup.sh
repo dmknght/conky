3e9eb92a4f4fef922ee733ff4f149b01491ba261
dmknght <dmknghtx2team@gmail.com>
2018-07-30 13:47:10 +0700

    skip error log

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sleep 5 &
-conky  -c /etc/conky/config0 &
-conky -c /etc/conky/config1 &
-conky -c /etc/conky/config2 &
+conky  -c /etc/conky/config0 2>/dev/null &
+conky -c /etc/conky/config1 2>/dev/null &
+conky -c /etc/conky/config2 2>/dev/null &
 exit
